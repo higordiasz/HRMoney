19afe0d2c7c42f0730703553841dee0050abe1eb
Dias <higordiaszuqueto@gmail.com>
2021-01-05 17:04:11 -0300

    alooo

--- src/controllers/log-controller.js
+++ src/controllers/log-controller.js
@@ -5,10 +5,16 @@ const Log = mongoose.model('Log');
 // list
 exports.senderLog = async (req, res) => {
   try {
+    
     let log = new Log(req.body);
+    
     log.save();
+    
     res.status(200).send({ message: 'Sucesso ao cadastrar o log!'})
+  
   } catch (e) {
+  
     res.status(500).send({ message: 'Erro ao salvar o Log: ' + e.message });
+  
   }
 };
