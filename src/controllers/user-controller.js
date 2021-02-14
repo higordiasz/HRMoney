44f06a634b2953a2f48e578de91949fec5812fa9
Dias <higordiaszuqueto@gmail.com>
2021-02-14 19:43:54 -0300

    5.2

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -95,6 +95,52 @@ exports.Create = async (req, res) => {
     }
 }
 
+exports.CreateSite = async (req, res) => {
+    try {
+
+        let email = req.body.email;
+
+        let Existe = await User.find({ Email: email })
+
+        if (Existe.length > 0)
+        return 2;
+        else {
+            if (req.body.pass == req.body.passre) {
+                let senhaCriptografada = md5(req.body.pass)
+                let Token = md5(req.body.email + req.body.pass)
+                const usuario = new User({
+                    Email: email,
+                    Senha: senhaCriptografada,
+                    Token: Token,
+                    Challenge: false,
+                    Delay_acao1: 15000,
+                    Delay_acao2: 20000,
+                    Delay_assistir: 50000,
+                    Delay_block: 3600000,
+                    Delay_ciclo: 600000,
+                    Delay_conta: 120000,
+                    Delay_meta: 3600000,
+                    Delay_perfil: 15000,
+                    Delay_rodar: 60000,
+                    Meta: 1000,
+                    Movimentador: false,
+                    Qtd: 30,
+                    Qtd_curtidas: 5,
+                    Adquirido: false
+                });
+
+                await usuario.save();
+
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    } catch (e) {
+        return 0;
+    }
+}
+
 exports.Create3 = async (Email, Senha) => {
     try {
 
--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -9,28 +9,42 @@ function CadastrarConta(Email, Senha) {
 };
 
 router.get('/', (req, res, next) => {
-  res.render('index', {page: 'Home', menuId: 'home'})
+  res.render('index', { page: 'Home', menuId: 'home' })
 });
 
 router.get('/packages', (req, res, next) => {
-  res.render('packages', {page: 'Home', menuId: 'home'})
+  res.render('packages', { page: 'Home', menuId: 'home' })
 });
 
 router.get('/download', async (req, res, next) => {
   try {
     let data = await linkController.getLinkInterno()
     if (data != null) {
-      res.render('download', {Mega: "<a href=\"" + data.Mega + "\" class=\"borders-btn\">Download</a>", Media: "<a href=\"" + data.Media + "\" class=\"borders-btn\">Download</a>", Drive: "<a href=\"" + data.Drive + "\" class=\"borders-btn\">Download</a>"})
+      res.render('download', { Mega: "<a href=\"" + data.Mega + "\" class=\"borders-btn\">Download</a>", Media: "<a href=\"" + data.Media + "\" class=\"borders-btn\">Download</a>", Drive: "<a href=\"" + data.Drive + "\" class=\"borders-btn\">Download</a>" })
     } else {
-      res.render('download', {Mega: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Media: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Drive: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>"})
+      res.render('download', { Mega: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Media: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Drive: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>" })
     }
   } catch {
-    res.render('download', {Mega: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Media: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Drive: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>"})
+    res.render('download', { Mega: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Media: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Drive: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>" })
   }
 });
 
 router.get('/registro', (req, res, next) => {
-  res.render('registro')
+  res.render('registro', { Cadastro: "" });
+});
+
+router.post('/registro', async (req, res, next) => {
+  var cadastro = await User.CreateSite(req, res);
+  if (cadastro == 1) {
+    res.render('registro', { Cadastro: "<p style=\"color:green\">Cadastro realizado com sucesso!</p>" })
+  } else {
+    if (cadastro == 2) {
+      res.render('registro', { Cadastro: "<p style=\"color:red\">Email ja cadastrado!</p>" })
+    }
+    else {
+      res.render('registro', { Cadastro: "<p style=\"color:red\">Erro ao realizar o cadastro!</p>" })
+    }
+  }
 });
 
 module.exports = router;
