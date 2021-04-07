7cb3b9374d9cc6d10cfd43e2a4f951d2a0db270d
Dias <higordiaszuqueto@gmail.com>
2021-04-06 21:04:28 -0300

    indexerror

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -10,8 +10,8 @@ function CadastrarConta(Email, Senha) {
   return User.Create3(Email, Senha)
 };
 
-router.get('/', (req, res, next) => {
-  res.render('index', { page: 'Home', menuId: 'home' })
+router.get('/', forwardAuthenticated, (req, res, next) => {
+  res.render('error-404', { page: 'Home', menuId: 'home' })
 });
 
 router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
@@ -25,25 +25,16 @@ router.post('/login', (req, res, next) => {
 });
 
 router.get('/packages', (req, res, next) => {
-  res.render('packages', { page: 'Home', menuId: 'home' })
+  
 });
 
 router.get('/download', async (req, res, next) => {
-  try {
-    let data = await linkController.getLinkInterno()
-    if (data != null) {
-      res.render('download', { Mega: "<a href=\"" + data.Mega + "\" class=\"borders-btn\">Download</a>", Media: "<a href=\"" + data.Media + "\" class=\"borders-btn\">Download</a>", Drive: "<a href=\"" + data.Drive + "\" class=\"borders-btn\">Download</a>" })
-    } else {
-      res.render('download', { Mega: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Media: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Drive: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>" })
-    }
-  } catch {
-    res.render('download', { Mega: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Media: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>", Drive: "<a href=\"" + "Erro API" + "\" class=\"borders-btn\">Download</a>" })
-  }
+  
 });
 
 router.get('/registro', forwardAuthenticated, (req, res) => res.render('register', {Cadastro: ""}));
 
-router.post('/registro', async (req, res, next) => {
+router.post('/registro', forwardAuthenticated, async (req, res, next) => {
   var cadastro = await User.CreateSite(req, res);
   if (cadastro == 1) {
     res.redirect('/login')
