b50a7e1576f134f9ac9f2b16775812c9c1e1653b
Dias <higordiaszuqueto@gmail.com>
2021-04-22 19:44:04 -0300

    SSl

--- src/app.js
+++ src/app.js
@@ -92,8 +92,7 @@ process.on('SIGINT', () => {
     });
 });
 
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
-    console.log('Oi')
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
     if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
         res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
     else //Se a requisição já é HTTPS 
